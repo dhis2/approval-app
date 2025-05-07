90b6cbf4923dd28ec852a101364e6436d758ed01
chauthutran <cthutran@gmail.com>
2025-05-07 17:10:08 +0900

    Add filter for category options for multiple cases

--- src/top-bar/attribute-combo-select/multiple-category-select.js
+++ src/top-bar/attribute-combo-select/multiple-category-select.js
@@ -1,35 +1,51 @@
 import i18n from '@dhis2/d2-i18n'
-import {
-    NoticeBox,
-    SingleSelectField,
-    SingleSelectOption,
-} from '@dhis2/ui'
+import { NoticeBox, SingleSelectField, SingleSelectOption } from '@dhis2/ui'
 import PropTypes from 'prop-types'
 import React, { useEffect } from 'react'
 import css from './category-option-select.module.css'
+import { useState } from 'react'
 
 /**
- * 
+ *
  * @param categoryCombo An object which has an array of category objects (JSON), each options (to be rendered in a menu).
  * @param orgUnit An object
  * @param selected {<categoryId_1>: <catOptionId_1>, <categoryId_2>: <catOptionId_2>, ...}
  * @param onChange A function to handle changes in the selected options.
  * @param onClose A function to close the menu.
- * 
+ *
  */
 export default function MultipleCategoySelect({
     categories,
     selected,
-    onChange
+    onChange,
 }) {
-    useEffect(() => {
-        
-    }, [selected])
-    
+    const [searchQueries, setSearchQueries] = useState({})
+
+    useEffect(() => {}, [selected])
+
+    // Filter categoryOptions based on the search query
+    const filterCategoryOptions = (categoryId, categoryOptions) => {
+        const searchKey = searchQueries[categoryId] || "";
+        return categoryOptions.filter(
+            (catOption) =>
+                catOption.displayName
+                    .toLowerCase()
+                    .includes(searchKey.toLowerCase())
+        )
+    }
+
+    const setSearchKey = (id, value) => {
+        setSearchQueries((prev) => ({
+            ...prev,
+            [id]: value,
+        }));
+
+    }
+
     return (
         <div className={css.inputs}>
             {/* Categories Dropdown */}
-            {categories.map(({ id, displayName, categoryOptions }) => 
+            {categories.map(({ id, displayName, categoryOptions }) =>
                 categoryOptions.length === 0 ? (
                     <NoticeBox
                         className={css.noOptionsBox}
@@ -42,26 +58,55 @@ export default function MultipleCategoySelect({
                         )}
                     </NoticeBox>
                 ) : (
-                    // Renders a SingleSelectField for each category
-                    <div className={css.inputWrapper} key={id}>
-                        <SingleSelectField
-                            label={displayName} // The category's displayName.
-                            selected={selected[id]} // Current selected option ID.
-                            onChange={({ selected }) => onChange(id, selected)}
-                        >
-                            {categoryOptions.map(({ id, displayName }) => (
-                                <SingleSelectOption
+                    <div className={css.filterGroup}>
+                        <label htmlFor={`search-${id}`} className={css.label}>
+                            {displayName}
+                        </label>
+
+                        <div className={css.inputWithDropdown}>
+                            <input
+                            id={`search-${id}`}
+                            type="text"
+                            placeholder={i18n.t('Search for {{categoryName}}', {
+                                categoryName: displayName,
+                                nsSeparator: '-:-',
+                            })}
+                            value={searchQueries[id] || ""}
+                            onChange={(e) => setSearchKey(id, e.target.value)}
+                            className={css.searchInput}
+                            />
+
+                            {filterCategoryOptions(id, categoryOptions).length === 0 ? (
+                            <div className={css.empty}>
+                                <span>
+                                {i18n.t('No results found for {{searchQuery}}', {
+                                    searchQuery: searchQueries[id],
+                                    nsSeparator: '-:-',
+                                })}
+                                </span>
+                            </div>
+                            ) : (
+                            <div className={css.dropdownWrapper}>
+                                <SingleSelectField
+                                selected={selected[id]}
+                                onChange={({ selected }) => onChange(id, selected)}
+                                >
+                                {filterCategoryOptions(id, categoryOptions).map(({ id, displayName }) => (
+                                    <SingleSelectOption
                                     key={id}
                                     value={id}
                                     label={displayName}
                                     className={css.dropdown}
-                                />
-                            ))}
-                        </SingleSelectField>
-                    </div>
+                                    />
+                                ))}
+                                </SingleSelectField>
+                            </div>
+                            )}
+                        </div>
+                        </div>
+
                 )
             )}
-            
         </div>
     )
 }
